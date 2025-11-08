f1aa3620cabc37ce40e262d8f5da5362af4a7b0a
Ankith <160985511+zaftus@users.noreply.github.com>
2025-11-07 20:56:33 -0500

    Create App.java

---
+++ src/main/App.java
@@ -0,0 +1,10 @@
+package main;
+
+import main.views.MainMenu;
+
+public class App {
+    public static void main(String[] args) {
+        MainMenu menu = new MainMenu();
+        menu.display();
+    }
+}
